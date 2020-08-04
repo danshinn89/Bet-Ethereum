07947adfc689377e6cb4d62616533fb2f15c3eef
Daniel Shinn <daniel.shinn89@gmail.com>
2020-08-04 16:56:30 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -14,10 +18,25 @@ module.exports = {
    
   ganache: {
     provider: function() {
+      return new HDWalletProvider("", "http://127.0.0.1:7545", MetaMaskAccountIndex)
       
   },
     network_id: 5777
   },
+
+  goerli: {
+    provider: function() {
+      return new HDWalletProvider("", "https://goerli.infura.io/v3/13f1920c404a4b338cd42ecf6ce3a485", MetaMaskAccountIndex)
+    },
+    network_id: 5
+  },
+  
+    ropsten: {
+    provider: function() {
+      return new HDWalletProvider("", "https://ropsten.infura.io/v3/13f1920c404a4b338cd42ecf6ce3a485", MetaMaskAccountIndex)
+    },
+    network_id: 3
+    },
 
     compilers: {
       solc: {
