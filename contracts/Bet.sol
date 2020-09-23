872cfee8a46a2447db0a42780854bcbb6cc995d9
Daniel Shinn <daniel.shinn89@gmail.com>
2020-09-23 11:48:59 -0400

    Update Bet.sol
    Allows user to create new game, Onlyadmin can payout

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,136 +1,160 @@
-pragma solidity >=0.5.16;
+pragma solidity >=0.5.3;
 
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
+contract SportsBet {
+    using SafeMath for uint256;
 
-contract Bet  {
+    address payable owner;
+    uint minBet;
     
-    address payable public owner;
-    uint pot;
-    uint minimumBet;
-    uint SetWinner = 0;
+    uint256 totalBetsOne;
+    uint256 totalBetsTwo;
+
+    address payable[] public players;
+    address public oracle;
+    
+    enum BettingStatus {Not_started, Started, Finished}
     
     
-    struct Payment{
-        
-        uint amount;
-        uint timestamp;
-      
+    BettingStatus public betStatus;
+    
+    
+    struct Game{
+    uint betId;  
     }
     
-    struct Player  {
-        
-        uint entry;
-        uint amountBet; 
-        uint numPayments;
-        bool teamA;
-        bool teamB;
-        bool collect;
-        mapping(uint => Payment) payments; // maps the payments to unassigned integers . a counter
-     
+    struct Player {
+    uint256 amountBet;
+    uint16 team;
+    bool betMade;
+    uint odds;
     }
     
-
-    mapping (address => Player ) public playerBet; //mapping adddress to the (struct)balance)
+    uint nextGameId;
     
+    mapping(address => Player) public playerInfo;
+    mapping(uint => Game) public gameInfo;
     
-    event PaymentSent(address _from, uint _amount);
-    event CheckWinner(uint _winner);
-    event CheckBool(bool _team);
+    event BetPlayer(address indexed _from, uint256 _amount, uint player);
     
-     constructor() public {
-      
-      owner = msg.sender;
-      minimumBet = 2000000000000000000;
-      pot = 2000000000000000000;
-      
-   
-    }    
-
-   
-   
-   function betPlayerOne() public payable {
-    //minimum required amount to enter  
-    require(msg.value >= minimumBet, "Minimum Tokens Required = 2");
-    require(SetWinner == 0, "Sorry you missed this one");
-    //takes how much the player has bet and sets it to their msg.value
-    playerBet[msg.sender].amountBet += msg.value; // .tatalbalance(access struct values)
-    playerBet[msg.sender].entry = 1;
-   
-    //records memory of payment made and timestamp
-    Payment memory payment = Payment(msg.value,now); //creating a new payment (stored in memory, msg.value amount sent / not --> gives current timestamp / Type payment created)
-    playerBet[msg.sender].payments[playerBet[msg.sender].numPayments] = payment; //baalanceReceived[by the address]--> calls payments[track what i just received from sender].numPayments(calls the struct to register that as payment= payment)
-    
-    //changes the values of Struct Player after selection is made
-    playerBet[msg.sender].teamA = true;
-    playerBet[msg.sender].entry = 1;
-    
-    
-   }
-    
-   function betPlayerTwo() public payable {
-    //minimum required amount to enter / Make sure a winner hasn't been declared
-    require(msg.value >= minimumBet, "Minimum Tokens Required = 2"); 
-    require(SetWinner == 0, "Sorry you missed this one");
-    //takes how much the player has bet and sets it to their msg.value
-    playerBet[msg.sender].amountBet += msg.value; // .tatalbalance(access struct values)
-    
-    //records memory of payment made and timestamp
-    Payment memory payment = Payment(msg.value,now); //creating a new payment (stored in memory, msg.value amount sent / not --> gives current timestamp / Type payment created)
-    playerBet[msg.sender].payments[playerBet[msg.sender].numPayments] = payment; //balanceReceived[by the address]--> calls payments[track what i just received from sender].numPayments(calls the struct to register that as payment= payment)
-    
-    //changes the values of Struct Player after selection is made
-    playerBet[msg.sender].teamB = true;
-    playerBet[msg.sender].entry = 2;
-    
-   }
-   
-   function payouts() public {
-     require(playerBet[msg.sender].entry == SetWinner, "You did not win");
-     require(playerBet[msg.sender].collect == false, "You already collected your winnings");
-     if(playerBet[msg.sender].entry == 1 && SetWinner ==1) {
-        playerBet[msg.sender].amountBet += pot;
-        playerBet[msg.sender].collect = true;
-        
-     }else{
-         playerBet[msg.sender].entry == 2 && SetWinner ==2;
-         playerBet[msg.sender].amountBet += pot;
-         playerBet[msg.sender].collect = true;
-         
-     }
-        
-     emit PaymentSent(msg.sender, pot); 
-     }
-   
-    
-    function makeWinnerA() public  {
-        require(msg.sender == owner, "Not the owner");
-        SetWinner = 1;
-        emit CheckWinner(SetWinner);
-       
-    }   
-       
-    function makeWinnerB() public   {
-        require(msg.sender == owner, "Not the owner");
-        SetWinner = 2;
-         
-        emit CheckWinner(SetWinner);
-    }   
-    
-    function withdrawTokens(uint _amount) public {
-        require(playerBet[msg.sender].amountBet > 0, "You have nothing to withdraw");
-        require(playerBet[msg.sender].entry == SetWinner, "You lost this one");
-        require(playerBet[msg.sender].collect == true, "Please collect your winnings");
-        _amount = (playerBet[msg.sender].amountBet);
-        msg.sender.transfer(_amount);
-        
+    constructor() public {
+        owner = msg.sender;
+        minBet = 1000000000000000000;
+        betStatus = BettingStatus.Not_started;
         
     }
     
-  //  function destroyContract() public onlyOwner {
-        
-        
+    function configureOracle(address _oracle) external onlyAdmin{
+        oracle = _oracle;  
+    }
     
+    function kill() public {
+      if(msg.sender == owner) selfdestruct(owner);
+    }
+    
+    function checkPlayerExists(address payable player) public view returns(bool){
+      for(uint256 i = 0; i < players.length; i++){
+         if(players[i] == player) return true;
+      }
+      return false;
+    }
+    
+    //creates game and gives it a betID
+    //changes bet status to started
+    function newGame() public  {
+        require(betStatus == BettingStatus.Not_started, "This game has already started");
+        gameInfo[nextGameId].betId = nextGameId;
+        betStatus = BettingStatus.Started;
+        nextGameId++;
+    }
+    
+    //users can make a bet on team 1 or team 2. 
+    function makeBet(uint8 _team, uint _odds) public payable {
+        require(betStatus == BettingStatus.Started,"Game has not been created");
+        require(playerInfo[msg.sender].betMade == false,"You have already made a bet");
+        require(msg.value >= minBet, "Not enough sent to bet");
         
-  // }
+        
+        //Player will make a bet and select team 
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].team = _team;
+        playerInfo[msg.sender].odds = _odds;
+        
+        //Add playerInfo to the players array
+        players.push(msg.sender);
+        
+        if(_team == 1){
+            totalBetsOne += msg.value;
+        }
+        else{
+            totalBetsTwo += msg.value;
+        }
+        playerInfo[msg.sender].betMade = true;
+        
+        emit BetPlayer(msg.sender, msg.value, _team);
+        
+    }
+    
+    function payouts(uint _winner) public onlyAdmin {
+        //temporary memory array. Fixed size to test
+        address payable[100] memory winners;
+        uint256 count = 0;
+        uint256 loserBet = 0;
+        uint256 winnerBet = 0;
+        address add;
+        uint256 playerOdds;
+        uint256 betPlaced;
+        address payable playerAddress;
+        
+        //loop through players to see who selected winning team
+        for(uint256 i = 0; i < players.length; i++){
+            playerAddress = players[i];
+        //players who selected winning team     
+        if(playerInfo[playerAddress].team == _winner){
+        winners[count] = playerAddress;
+        count++;
+        
+        }
+    }
+    if(_winner == 1){
+        loserBet = totalBetsTwo;
+        winnerBet = totalBetsOne;
+        
+    }else{
+        loserBet = totalBetsOne;
+        winnerBet = totalBetsTwo;
+        
+    }
+    
+    //check winners array & pays out winners
+    for(uint256 j = 0; j < count; j++){
+        if(winners[j] != address(0))
+        add = winners[j];
+        betPlaced = playerInfo[add].amountBet;
+        playerOdds = playerInfo[add].odds;
+        winners[j].transfer((betPlaced));
+    }
+    
+    //reset data
+    delete playerInfo[playerAddress];
+    loserBet = 0;
+    winnerBet = 0;
+    totalBetsOne = 0;
+    totalBetsTwo = 0;
+    
+}
+    
+    function teamOne() public view returns(uint256) {
+        return totalBetsOne;
+    }
+    
+    function teamTwo() public view returns(uint256) {
+        return totalBetsTwo;
+    }
+    
+    modifier onlyAdmin(){
+        require(msg.sender == owner);
+        _;
+    }
 }
