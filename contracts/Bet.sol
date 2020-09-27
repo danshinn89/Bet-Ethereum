9f423e7fd26894c4067f9d03b2b31c4abe6b0083
Daniel Shinn <daniel.shinn89@gmail.com>
2020-09-26 23:13:30 -0400

    Bet.sol
    Updating functionality. Stores game so multiple can be played. Users cannot bet on a match that has not been created. Admin pays out winners.

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -11,27 +11,27 @@ contract SportsBet {
     uint256 totalBetsOne;
     uint256 totalBetsTwo;
 
-    address payable[] public players;
+    
     address public oracle;
     
-    enum BettingStatus {Not_started, Started, Finished}
+    enum PlayerStatus {Not_Joined, Joined, Ended}
+    enum State {Not_Created, Created, Joined, Finished}
     
-    
-    BettingStatus public betStatus;
-    
-    
-    struct Game{
+    struct Game {
     uint betId;  
+    State state;
+    
     }
     
     struct Player {
+    address payable players;
     uint256 amountBet;
     uint16 team;
-    bool betMade;
     uint odds;
+    PlayerStatus _state;
     }
     
-    uint nextGameId;
+    uint public gameId;
     
     mapping(address => Player) public playerInfo;
     mapping(uint => Game) public gameInfo;
@@ -41,7 +41,7 @@ contract SportsBet {
     constructor() public {
         owner = msg.sender;
         minBet = 1000000000000000000;
-        betStatus = BettingStatus.Not_started;
+      
         
     }
     
@@ -49,40 +49,29 @@ contract SportsBet {
         oracle = _oracle;  
     }
     
-    function kill() public {
+    function kill() public onlyAdmin {
       if(msg.sender == owner) selfdestruct(owner);
     }
-    
-    function checkPlayerExists(address payable player) public view returns(bool){
-      for(uint256 i = 0; i < players.length; i++){
-         if(players[i] == player) return true;
-      }
-      return false;
-    }
-    
-    //creates game and gives it a betID
-    //changes bet status to started
-    function newGame() public  {
-        require(betStatus == BettingStatus.Not_started, "This game has already started");
-        gameInfo[nextGameId].betId = nextGameId;
-        betStatus = BettingStatus.Started;
-        nextGameId++;
+
+    function newGame() external  onlyAdmin {
+        
+        gameInfo[gameId] = Game(gameId, State.Created);
+        gameId++;
+        
     }
     
     //users can make a bet on team 1 or team 2. 
-    function makeBet(uint8 _team, uint _odds) public payable {
-        require(betStatus == BettingStatus.Started,"Game has not been created");
-        require(playerInfo[msg.sender].betMade == false,"You have already made a bet");
-        require(msg.value >= minBet, "Not enough sent to bet");
+    function makeBet(uint _gameId, uint8 _team, uint _odds) external payable {
         
+        Game storage game = gameInfo[_gameId];
+        require(game.state == State.Created,"Game has not been created");
+        require(playerInfo[msg.sender]._state == PlayerStatus.Not_Joined, "You have already placed a bet");
+        require(msg.value >= minBet, "Not enough sent to bet");
         
         //Player will make a bet and select team 
         playerInfo[msg.sender].amountBet = msg.value;
         playerInfo[msg.sender].team = _team;
         playerInfo[msg.sender].odds = _odds;
-        
-        //Add playerInfo to the players array
-        players.push(msg.sender);
         
         if(_team == 1){
             totalBetsOne += msg.value;
@@ -90,61 +79,67 @@ contract SportsBet {
         else{
             totalBetsTwo += msg.value;
         }
-        playerInfo[msg.sender].betMade = true;
-        
+         
+        playerInfo[msg.sender]._state = PlayerStatus.Joined;
         emit BetPlayer(msg.sender, msg.value, _team);
         
     }
+
     
-    function payouts(uint _winner) public onlyAdmin {
-        //temporary memory array. Fixed size to test
+   function pay(uint _gameId, uint _winner) public onlyAdmin {
+        Game storage game = gameInfo[_gameId];
+        
         address payable[100] memory winners;
         uint256 count = 0;
         uint256 loserBet = 0;
         uint256 winnerBet = 0;
         address add;
-        uint256 playerOdds;
+   //     uint256 playerOdds;
         uint256 betPlaced;
         address payable playerAddress;
         
         //loop through players to see who selected winning team
-        for(uint256 i = 0; i < players.length; i++){
-            playerAddress = players[i];
+        for(uint256 i = 0; i < winners.length; i++){
+            playerAddress = winners[i];
+            
         //players who selected winning team     
         if(playerInfo[playerAddress].team == _winner){
         winners[count] = playerAddress;
         count++;
-        
         }
-    }
-    if(_winner == 1){
-        loserBet = totalBetsTwo;
-        winnerBet = totalBetsOne;
         
-    }else{
-        loserBet = totalBetsOne;
-        winnerBet = totalBetsTwo;
+            
+        }
+       
+       if ( _winner == 1) {
+           loserBet = totalBetsTwo;
+           winnerBet = totalBetsOne;
+       }else{
+           loserBet = totalBetsTwo;
+           winnerBet = totalBetsOne;
+       }
+       
+       for(uint256 j = 0; j < count; j++){
+       if(winners[j] != address(0))
+       add = winners[j];
+       betPlaced = playerInfo[add].amountBet;
+   //    playerOdds = playerInfo[add].odds;
+       winners[j].transfer((betPlaced*(10000+(loserBet*10000/winnerBet)))/10000);
+       
+       }
+       //reset data
+       delete playerInfo[playerAddress];
+       loserBet = 0;
+       winnerBet = 0;
+       totalBetsOne = 0;
+       totalBetsTwo = 0;
+       
+       game.state == State.Finished;
         
+    
     }
-    
-    //check winners array & pays out winners
-    for(uint256 j = 0; j < count; j++){
-        if(winners[j] != address(0))
-        add = winners[j];
-        betPlaced = playerInfo[add].amountBet;
-        playerOdds = playerInfo[add].odds;
-        winners[j].transfer((bet*(10000+(loserBet*10000/winnerBet)))/10000 );
-    }
-    
-    //reset data
-    delete playerInfo[playerAddress];
-    loserBet = 0;
-    winnerBet = 0;
-    totalBetsOne = 0;
-    totalBetsTwo = 0;
-    
-}
-    
+   
+ 
     function teamOne() public view returns(uint256) {
         return totalBetsOne;
     }
